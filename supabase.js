852963e66c5917c6974446e8e80f1a559262765c
klefab0303 <klefab0303@gho365.de>
2026-02-21 20:27:17 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,8 +2,8 @@
 // Verbindet die App mit deiner Supabase-Datenbank.
 // ERSETZE die beiden Werte unten mit deinen eigenen Daten (Schritt-für-Schritt-Anleitung weiter unten).
 
-const SUPABASE_URL = 'https://utgntktnvnayksksqlyg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Z250a3Rudm5heWtza3NxbHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MTYzNzEsImV4cCI6MjA4NzA5MjM3MX0.nSstbmb5FMQor-5WJoFjxfWRrVsWtgp-LrkNmr3wNiM';
+const SUPABASE_URL = 'https://qibmzwckrnkwpgvwosuk.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYm16d2Nrcm5rd3Bndndvc3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2OTQ1NjQsImV4cCI6MjA4NzI3MDU2NH0.rde0zrU5ACAZDcCaNGe0QXnkEgSBhaTZeNXxB3-YMzY';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
